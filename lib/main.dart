21d258a0bfff935729fdce9f6d989986acef93bb
Marcelo M. Silva Ferreira da Conceicao <marcelo.conceicao@ibametro.local>
2026-07-08 16:24:49 -0300

    Upload Project

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'MeuAplicativo.dart';
+
+void main() {
+  runApp(const MeuAplicativo());
+}
---
+++ lib/pages/moedas_pages.dart
@@ -0,0 +1,56 @@
+import 'package:app/repositories/moeda_repository.dart';
+import 'package:flutter/material.dart';
+
+class MoedasPage
+    extends StatelessWidget {
+  const MoedasPage({super.key});
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    final tabela =
+        MoedaRepository.tabela;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Cripto Moedas',
+        ),
+      ),
+      body: ListView.separated(
+        itemBuilder:
+            (
+              BuildContext context,
+              int moeda,
+            ) {
+              return ListTile(
+                leading:
+                    Image.asset(
+                      tabela[moeda]
+                          .icone,
+                    ),
+                title: Text(
+                  tabela[moeda]
+                      .nome,
+                ),
+                trailing: Text(
+                  tabela[moeda]
+                      .preco
+                      .toString(),
+                ),
+              );
+            },
+        padding: EdgeInsets.all(
+          16,
+        ),
+        separatorBuilder:
+            (_, ___) => Divider(),
+        itemCount: tabela.length,
+      ),
+      floatingActionButton:
+          FloatingActionButton(
+            onPressed: () {},
+          ),
+    );
+  }
+}
